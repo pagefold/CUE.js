3638d554202cce8df6a1b1e257622cd29f5fa1ab
Brandon McKinney <pagefold@gmail.com>
2012-04-12 14:13:21 -0500

    Make sure CUE is global and remove redundant var statement in inner block.

--- CUE.js
+++ CUE.js
@@ -6,36 +6,39 @@
  *   by Brandon McKinney <pagefold@gmail.com>
  *   https://github.com/pagefold/CUE.js
  */
-var CUE = function(){
-  // Store a reference to document.head locally.
-  var head = document.getElementsByTagName('head')[0];
+(function(){
+  window.CUE = function(){
+    // Store a reference to document.head locally.
+    var head = document.getElementsByTagName('head')[0];
 
-  // Create a public Array to hold the queue.
-  this.cue = [];
+    // Create a public Array to hold the queue.
+    this.cue = [];
 
-  this.push = function(){
-    // Add the arguments to a stack if a string or function. Others are ignored.
-    for (i in arguments) { if (/^(string|function)$/.test(typeof arguments[i]))  this.cue.push(arguments[i]); }
+    this.push = function(){
+      // Add the arguments to a stack if a string or function. Others are ignored.
+      for (i in arguments) { if (/^(string|function)$/.test(typeof arguments[i]))  this.cue.push(arguments[i]); }
+    };
+
+    // Process the queue using recursion until the cue array is empty.
+    this.go = function() {
+      var that = this,
+          js = this.cue.shift(),
+          s = null;
+      if (typeof js == 'string') {
+          s = document.createElement('script');
+          s.src = js;
+          console.log(js);
+          s.onload = function(){ that.go() };
+          s.onreadystatechange = function(){ if (/^(complete|loaded)$/i.test(s.readyState)) that.go() };
+          head.appendChild(s);
+      } else if (typeof js == 'function') {
+          js.call(window);
+          this.go();
+      }
+      return this;
+    };
+    
+    // Process anything passed in the initialization of the class.
+    this.push.apply(this, arguments);
   };
-
-  // Process the queue using recursion until the cue array is empty.
-  this.go = function() {
-    var that = this,
-        js = this.cue.shift();
-    if (typeof js == 'string') {
-        var s = document.createElement('script');
-        s.src = js;
-        console.log(js);
-        s.onload = function(){ that.go() };
-        s.onreadystatechange = function(){ if (/^(complete|loaded)$/i.test(s.readyState)) that.go() };
-        head.appendChild(s);
-    } else if (typeof js == 'function') {
-        js.call(window);
-        this.go();
-    }
-    return this;
-  };
-  
-  // Process anything passed in the initialization of the class.
-  this.push.apply(this, arguments);
-};
+})();
